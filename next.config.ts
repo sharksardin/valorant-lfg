8feb363ad2caa2f21ba965e820161ffdd1d14e32
sharksardin <4874rezzo@gmail.com>
2026-08-23 17:09:07 +0900

    fix: next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,5 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-module.exports = {
-  allowedDevOrigins: ["100.109.6.108"],
-};
 
 export default nextConfig;
